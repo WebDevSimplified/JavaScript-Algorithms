72f573fe41fa47c1d05a22f9869d32357c89c966
Soubik Bhui <73220513+sb16072007@users.noreply.github.com>
2020-12-30 13:14:38 +0530

    Updated ./Way Too Long Words/script.js

--- Way Too Long Words/script.js
+++ Way Too Long Words/script.js
@@ -1,3 +1,17 @@
+/*
+From CodeForces - 
+
+Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
+
+Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
+
+This abbreviation is made like this: we write down the first and the last letter of a word and between them we write the number of letters between the first and the last letters. That number is in decimal system and doesn't contain any leading zeroes.
+
+Thus, "localization" will be spelt as "l10n", and "internationalization» will be spelt as "i18n".
+
+You are suggested to automatize the process of changing the words with abbreviations. At that all too long words should be replaced by the abbreviation and the words that are not too long should not undergo any changes.
+*/
+
 function wayTooLongWords(word){
     let wFirst = word[0], wLast = word[word.length - 1];
     let len = word.length - 2;
